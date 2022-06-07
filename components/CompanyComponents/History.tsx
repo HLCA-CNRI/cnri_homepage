02307c36229c051efba096a52b4622e0a63b1413
Che Rin <yu24c@mtholyoke.edu>
2022-06-07 18:07:33 +0900

    lint fix

--- components/CompanyComponents/History.tsx
+++ components/CompanyComponents/History.tsx
@@ -42,7 +42,7 @@ const History = ({year}:HistoryType) => {
        <button onClick = {clickButton} className = "mb-3 text-3xl font-semibold tracking-tight text-white flex w-[100%] justify-center hover:rotate-45">+</button>
        {isActive ? 
      <div className = "text-white">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem 
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem 
      </div>:
      ""
      }

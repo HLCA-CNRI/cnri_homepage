faf88b8139a56170e4568ba8cd2257b31b661d6e
seahahn <seah.ahn.nt@gmail.com>
2022-06-15 16:26:38 +0900

    Partnership bottom container error fixed

--- components/RAndDCompoenents/Partnership.tsx
+++ components/RAndDCompoenents/Partnership.tsx
@@ -7,9 +7,7 @@ import ImgContainer from "./ImgContainer/ImgContainer";
 
 const Partnership = () => {
   const [userInteracted, setUserInteracted] = useState(true);
-  const [currentObj, setCurrentObj] = useState(
-    WorkAndPartnershipContent.totalBusiness
-  );
+  const [currentObj, setCurrentObj] = useState(WorkAndPartnershipContent.totalBusiness);
   const picRef = useRef<null | HTMLDivElement>(null);
 
   useEffect(() => {
@@ -50,14 +48,10 @@ const Partnership = () => {
   console.log(typeof currentObj.img);
 
   return (
-    <div className="grid grid-cols-3 w-[100%] border-2 h-[90vh]">
-      <div className="grid grid-cols-5">
+    <div className="grid grid-cols-3 w-[100%] h-[90vh]">
+      <div className="grid grid-cols-5 h-[90vh]">
         <div className="col-span-2"></div>
-        <div
-          onMouseOver={MouseOver}
-          onMouseLeave={MouseOut}
-          onClick={clickIcon}
-        >
+        <div onMouseOver={MouseOver} onMouseLeave={MouseOut} onClick={clickIcon}>
           <Icon
             defaultIcon={WorkAndPartnershipContent.totalBusiness.default}
             coloredIcon={WorkAndPartnershipContent.totalBusiness.colored}
@@ -66,11 +60,7 @@ const Partnership = () => {
           />
         </div>
         <div className="col-span-2"></div>
-        <div
-          onMouseOver={MouseOver}
-          onMouseLeave={MouseOut}
-          onClick={clickIcon}
-        >
+        <div onMouseOver={MouseOver} onMouseLeave={MouseOut} onClick={clickIcon}>
           <Icon
             defaultIcon={WorkAndPartnershipContent.ts.default}
             coloredIcon={WorkAndPartnershipContent.ts.colored}
@@ -80,11 +70,7 @@ const Partnership = () => {
         </div>
         <div className="col-span-3"></div>
 
-        <div
-          onMouseOver={MouseOver}
-          onMouseLeave={MouseOut}
-          onClick={clickIcon}
-        >
+        <div onMouseOver={MouseOver} onMouseLeave={MouseOut} onClick={clickIcon}>
           <Icon
             defaultIcon={WorkAndPartnershipContent.posco.default}
             coloredIcon={WorkAndPartnershipContent.posco.colored}
@@ -103,11 +89,7 @@ const Partnership = () => {
 
         <div className="col-span-5  h-8"></div>
         <div className="col-span-1"></div>
-        <div
-          onMouseOver={MouseOver}
-          onMouseLeave={MouseOut}
-          onClick={clickIcon}
-        >
+        <div onMouseOver={MouseOver} onMouseLeave={MouseOut} onClick={clickIcon}>
           <Icon
             defaultIcon={WorkAndPartnershipContent.h2.default}
             coloredIcon={WorkAndPartnershipContent.h2.colored}
@@ -117,11 +99,7 @@ const Partnership = () => {
         </div>
         <div className="col-span-1"></div>
 
-        <div
-          onMouseOver={MouseOver}
-          onMouseLeave={MouseOut}
-          onClick={clickIcon}
-        >
+        <div onMouseOver={MouseOver} onMouseLeave={MouseOut} onClick={clickIcon}>
           <Icon
             defaultIcon={WorkAndPartnershipContent.snu.default}
             coloredIcon={WorkAndPartnershipContent.snu.colored}
@@ -132,30 +110,19 @@ const Partnership = () => {
         <div className="col-span-1"></div>
       </div>
 
-
-        <div
-          ref={picRef}
-          onMouseEnter={MouseOver}
-          onMouseLeave={MouseOut}
-          className="bg-slate-200 flex flex-col col-span-2 px-10"
-        >
-          <div className="text-4xl pb-4 border-2 h-20">{currentObj.kTitle}</div>
-          <div className=" h-[80%] w-[90%] border-4 border-cyan-200 flex justify-center self-center xl:h-[60%]">
-            {currentObj.hasMultiple ? (
-              <Carousel values={currentObj.img}></Carousel>
-            ) : (
-              currentObj.img
-            )}
-          </div>
-          <div className="text-lg h-40 border-4 border-red-300 ">
-            <ul>
-              {currentObj.content.map((note: string) => (
-                <li key={note}>{note}</li>
-              ))}
-            </ul>
-          </div>
+      <div ref={picRef} onMouseEnter={MouseOver} onMouseLeave={MouseOut} className="bg-slate-200 flex flex-col col-span-2 px-10 h-[90vh]">
+        <div className="text-4xl pb-4 h-20">{currentObj.kTitle}</div>
+        <div className="h-[80%] w-[90%] flex justify-center self-center xl:h-[60%]">
+          {currentObj.hasMultiple ? <Carousel values={currentObj.img}></Carousel> : currentObj.img}
         </div>
-
+        <div className="text-lg h-40">
+          <ul>
+            {currentObj.content.map((note: string) => (
+              <li key={note}>{note}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };

aeac7e554c37a218ffd30a9857750386d4507e9d
Che Rin <yu24c@mtholyoke.edu>
2022-06-22 01:54:50 +0900

    fixed carousel img

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -24,27 +24,30 @@ const Carousel = ({ currentObj }: CarouselType) => {
   return (
     <>
       <div className=" hidden sm:flex text-[5.5vw]   sm:text-4xl sm:pb-4 sm:h-20  justify-start ">{currentObj.kTitle[current]}</div>
-      <div className=" ">
+     <div>
+     <div >
         <div className="flex justify-center self-center">
-
-          <div className = " w-[100vw] object-contain">{currentObj.img[current]}</div>
-
-        </div>
-       
-      </div>
-      <div className = "h-[10vw] sm:w-[90%]  grid grid-cols-2">
-         <button onClick={clickPrev} className = "border-2 bg-slate-100">
+        <button onClick={clickPrev} className = "w-[20%]">
             <div className="w-[100%] ">
               <img className="w-8" src="./images/buttonImg/leftBtn.png"></img>
             </div>
           </button>
-          <button  onClick={clickNext} className = "border-2 bg-slate-100">
+
+          <div className = "max-h-[30vh] max-w-[80%] object-contain">{currentObj.img[current]}</div>
+
+          <button  onClick={clickNext} className = "w-[20%]">
             <div className="w-[100%] flex justify-end">
               <img className="w-8" src="./images/buttonImg/rightBtn.png"></img>
             </div>
           </button>
-
+        </div>
+    
+       
       </div>
+
+     </div>
+     
+     
       <div className="  sm:hidden text-[5.5vw]  font-semibold  sm:pb-4 sm:h-20  justify-start pb-[1vh] pt-[2vh]">{currentObj.kTitle[current]}</div>
       {/* font size display size sm이때 안매겨짐  */}
       <div className="  hidden md:flex md:text-md  pt-4   ">

f37e6ff15b6a90dfe22363b9f43d78a85f6a9e85
Che Rin <yu24c@mtholyoke.edu>
2022-06-30 18:18:28 +0900

    ignore lint

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
 const withVideos = require("next-videos");
 
 module.exports = withVideos();
+

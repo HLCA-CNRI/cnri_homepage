add892932476d7dfdf59b21f8ba6c0f1865cfd2f
Che Rin <yu24c@mtholyoke.edu>
2022-07-06 18:20:44 +0900

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://cnrikorea.com",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
+
+export default config;

98b1c197aef5bc61a471e914ecd2b7df75ff4650
mnmhbbb <mnmhb64@gmail.com>
2022-08-10 15:27:26 +0900

    ✏️ (temp) product url modified

--- components/LayoutComponents/Hamburger/HamburgerMenu.tsx
+++ components/LayoutComponents/Hamburger/HamburgerMenu.tsx
@@ -77,7 +77,7 @@ function HamburgerMenu() {
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
-                  href="https://ciet.cnrikorea.com"
+                  href="https://ciet-test.cnrikorea.com"
                   className="  py-[2vh] text-[3vw] flex justify-between cursor-pointer hover:text-blue-600 hover:text-[3.5vw] w-[100%] px-[5vw]"
                   onClick={clickBurger}>
                   <div>PRODUCT</div>

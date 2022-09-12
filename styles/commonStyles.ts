b63b1032a233f23c354a6453243b44abab02f280
c-jeongyyun <99jenny0614@naver.com>
2022-09-12 14:18:54 +0900

    ✨ make styled-components about title, subtitle

---
+++ styles/commonStyles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const SectionTitleStyle = styled.div`
+  font-size: 10vw;
+  font-weight: 600;
+  margin-bottom: 2vh;
+  @media only screen and (min-width: 640px) {
+    font-size: 5vw;
+  }
+  @media only screen and (min-width: 768px) {
+    font-size: 4vw;
+  }
+  @media only screen and (min-width: 1536px) {
+    font-size: 80px;
+  }
+`;
+
+export const SectionSubTitleStyle = styled.div`
+  margin-bottom: 2vh;
+  @media only screen and (min-width: 640px) {
+    font-size: 2vw;
+  }
+  @media only screen and (min-width: 768px) {
+    font-size: 3vw;
+    margin-bottom: 5vh;
+  }
+  @media only screen and (min-width: 1536px) {
+    font-size: 40px;
+  }
+`;

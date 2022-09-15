af655a818337bd0ea38b7687d5a88f4775668c12
c-jeongyyun <99jenny0614@naver.com>
2022-09-15 11:53:20 +0900

    ✨ add rotate animation when clicked

--- components/CompanyComponents/HistoryComp.tsx
+++ components/CompanyComponents/HistoryComp.tsx
@@ -1,17 +1,21 @@
-import {size} from "lodash";
+import {motion, useAnimation} from "framer-motion";
 import {memo, useState} from "react";
 import styled from "styled-components";
 import HistoryContent from "../../functions/HistoryContents";
+import TransitionVariants from "../../functions/TransitionVariants";
 
 interface HistoryCompType {
   year: number; // HistoryContent Object에서 특정 year을 key로 사용
 }
 
 function HistoryComp({year}: HistoryCompType) {
+  const controlHistoryBtn = useAnimation();
   // 카드가 펼쳐져있는지 확인하는 state
   const [isActive, setIsActive] = useState(false);
   // 카드 펼치고 닫기 기능 구현 --> button에 onClick으로 사용 됨
   const clickButton = () => {
+    isActive ? controlHistoryBtn.start("rotateCW") : controlHistoryBtn.start("initial");
+
     setIsActive(!isActive);
   };
 
@@ -40,15 +44,11 @@ function HistoryComp({year}: HistoryCompType) {
           {year}
         </h4>
         {/* 카드 펼쳐져있지 않으면 + 펼쳐져있으면 X */}
-        {isActive ? (
-          <Button type="button" onClick={clickButton} size={"30px"}>
+        <Button onClick={clickButton}>
+          <motion.div animate={controlHistoryBtn} variants={TransitionVariants.RotateHistoryIcon}>
             ×
-          </Button>
-        ) : (
-          <Button type="button" onClick={clickButton} size={"45px"}>
-            +
-          </Button>
-        )}
+          </motion.div>
+        </Button>
 
         {/* 펼쳐져있으면 내용 사져와서 내열하기 */}
         {isActive ? (
@@ -87,6 +87,6 @@ const Button = styled.div`
   width: 100%;
 
   @media only screen and (min-width: 1024px) {
-    font-size: ${({size}) => size};
+    font-size: 45px;
   }
 `;

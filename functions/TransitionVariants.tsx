ed1dc67ab3419989e994926649eb6c69b599afc8
c-jeongyyun <99jenny0614@naver.com>
2022-09-19 17:19:43 +0900

    ✨ update transition varients

--- functions/TransitionVariants.tsx
+++ functions/TransitionVariants.tsx
@@ -5,7 +5,7 @@ const TransitionVariants = {
     vidHidden: {opacity: 0, x: -250},
   },
   TansitionVidRToL: {
-    vidVisible: {opacity: 1, x: 0, transition: {duration: 0.6}},
+    vidVisible: {opacity: 1, x: 100, transition: {duration: 0.6}},
     vidHidden: {opacity: 0, x: 100},
   },
 
@@ -15,9 +15,9 @@ const TransitionVariants = {
   },
 
   TransitionContextLToR: {
-    initial: {opacity: 0, x: -200, transition: {duration: 0.6}},
-    open: {opacity: 1, x: 0, transition: {duration: 0.6}},
-    close2: {x: -400, opacity: 0, transition: {duration: 0.6}},
+    initial: {opacity: 0, x: 0, transition: {duration: 0.6}},
+    open: {opacity: 1, x: "40%", transition: {duration: 0.6}},
+    close2: {x: 0, opacity: 0, transition: {duration: 0.6}},
   },
 
   TransitionContextLToR2: {
@@ -27,11 +27,10 @@ const TransitionVariants = {
   },
 
   TransitionContextRToL: {
-    initial: {opacity: 0, x: 200, transition: {duration: 0.6}},
-    open: {opacity: 1, x: 0, transition: {duration: 0.6}},
-    close2: {x: 400, opacity: 0, transition: {duration: 0.6}},
+    initial: {opacity: 1, x: 0, transition: {duration: 0.6}},
+    open: {opacity: 1, x: "-50%", transition: {duration: 0.6}},
+    close2: {x: 0, opacity: 1, transition: {duration: 0.6}},
   },
-
   TransitionContextTToB: {
     initial: {z: 0, opacity: 0, y: -200, transition: {duration: 0.6}},
     open: {z: 0, opacity: 1, y: 0, transition: {duration: 0.6}},

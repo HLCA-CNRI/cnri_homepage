48b450426e82177811516e751887d5ace63af3fe
c-jeongyyun <99jenny0614@naver.com>
2022-09-19 13:53:48 +0900

    ✨ make reduce cards layout

---
+++ components/LandingComponents.tsx/ReduceCards.tsx
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+import colors from "../../constants/colors";
+import LandingContents from "../../functions/LandingContents";
+import ReduceCard from "./ReduceCard";
+
+const ReduceCards = () => {
+  const [curIdx, setCurIdx] = useState();
+  const onCardClick = (e: any) => {
+    console.log(e.currentTarget.dataset.idx);
+    setCurIdx(e.currentTarget.dataset.idx);
+  };
+  return (
+    <div>
+      <div className="flex flex-row w-full mx-[10px]">
+        <div role="button" data-idx="0" onClick={onCardClick} className="mx-[20px]">
+          <ReduceCard
+            isOpened={curIdx === "0"}
+            width={660}
+            height={300}
+            contents={LandingContents.Reduce.content1}
+            bgColor={colors.CNRI_LIGHT_GREEN}
+          />
+        </div>
+        <div role="button" data-idx="1" onClick={onCardClick} className="mx-[20px]">
+          <ReduceCard
+            isOpened={curIdx === "1"}
+            width={660}
+            height={300}
+            contents={LandingContents.Reduce.content2}
+            bgColor={colors.CNRI_DARK_GREEN}
+          />
+        </div>
+      </div>
+      <div role="button" data-idx="2" onClick={onCardClick} className="flex mx-[20px] mt-[40px]">
+        <ReduceCard
+          isOpened={curIdx === "2"}
+          width={1400}
+          height={300}
+          contents={LandingContents.Reduce.content3}
+          bgColor={colors.CNRI_DARK_BLUE}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ReduceCards;

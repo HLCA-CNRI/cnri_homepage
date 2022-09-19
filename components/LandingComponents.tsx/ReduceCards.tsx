a56ac02706511fe3a33e7968181ee11d2bbc3e64
c-jeongyyun <99jenny0614@naver.com>
2022-09-19 14:33:55 +0900

    ✨ update reduce card click event

--- components/LandingComponents.tsx/ReduceCards.tsx
+++ components/LandingComponents.tsx/ReduceCards.tsx
@@ -6,24 +6,24 @@ import ReduceCard from "./ReduceCard";
 const ReduceCards = () => {
   const [curIdx, setCurIdx] = useState();
   const onCardClick = (e: any) => {
-    console.log(e.currentTarget.dataset.idx);
-    setCurIdx(e.currentTarget.dataset.idx);
+    const clickedIdx = e.currentTarget.dataset.idx;
+    setCurIdx((cur) => (cur === clickedIdx ? 0 : clickedIdx));
   };
   return (
     <div>
       <div className="flex flex-row w-full mx-[10px]">
-        <div role="button" data-idx="0" onClick={onCardClick} className="mx-[20px]">
+        <div role="button" data-idx="1" onClick={onCardClick} className="mx-[20px]">
           <ReduceCard
-            isOpened={curIdx === "0"}
+            isOpened={curIdx === "1"}
             width={660}
             height={300}
             contents={LandingContents.Reduce.content1}
             bgColor={colors.CNRI_LIGHT_GREEN}
           />
         </div>
-        <div role="button" data-idx="1" onClick={onCardClick} className="mx-[20px]">
+        <div role="button" data-idx="2" onClick={onCardClick} className="mx-[20px]">
           <ReduceCard
-            isOpened={curIdx === "1"}
+            isOpened={curIdx === "2"}
             width={660}
             height={300}
             contents={LandingContents.Reduce.content2}
@@ -31,9 +31,9 @@ const ReduceCards = () => {
           />
         </div>
       </div>
-      <div role="button" data-idx="2" onClick={onCardClick} className="flex mx-[20px] mt-[40px]">
+      <div role="button" data-idx="3" onClick={onCardClick} className="flex mx-[20px] mt-[40px]">
         <ReduceCard
-          isOpened={curIdx === "2"}
+          isOpened={curIdx === "3"}
           width={1400}
           height={300}
           contents={LandingContents.Reduce.content3}

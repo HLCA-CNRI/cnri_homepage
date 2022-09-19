2a55dfa97a4e001d6a90de02eb40a521f64e77e1
c-jeongyyun <99jenny0614@naver.com>
2022-09-19 15:33:26 +0900

    ✨ make remove card

---
+++ components/LandingComponents.tsx/RemoveCard.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+import {LandingSubContentsType} from "../../functions/LandingContents";
+
+const RemoveCard = ({
+  contents,
+  isOpened,
+}: {
+  contents: LandingSubContentsType;
+  isOpened: boolean;
+}) => (
+  <div className=" mx-[20px]">
+    <div className="w-[670px] h-[300px] relative">
+      <Image
+        src={contents.srcPath}
+        alt="remove img"
+        layout="fill"
+        className={`w-full h-full object-cover rounded-forImg  `}
+      />
+    </div>
+    <div
+      className={` ${
+        isOpened ? "animate-fadein" : "animate-fadeout"
+      } flex flex-col items-center justify-center `}>
+      <div className="text-[34px] font-bold border-t-[2px] border-slate-600 w-[95%] text-center pt-8 pb-4 mt-7">
+        {contents.title}
+      </div>
+      <div className="text-[20px] w-[83%] ">{contents.content}</div>
+    </div>
+  </div>
+);
+
+export default RemoveCard;

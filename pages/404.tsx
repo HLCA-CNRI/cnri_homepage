d386927674bd68fd4f0c9339f1e630ca39e9ef77
c-jeongyyun <99jenny0614@naver.com>
2022-09-27 13:52:49 +0900

    ✨ make error pages

---
+++ pages/404.tsx
@@ -0,0 +1,4 @@
+import Error from "../components/Error";
+
+const Custom404 = () => <Error errCode="404" text="페이지를 찾을 수 없습니다 :(" />;
+export default Custom404;
---
+++ pages/503.tsx
@@ -0,0 +1,4 @@
+import Error from "../components/Error";
+
+const Custom503 = () => <Error errCode="503" text="서버에 문제가 발생했습니다 :(" />;
+export default Custom503;

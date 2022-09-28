78a227c97bde0c90fbcb7d739b50ca885ce316d7
c-jeongyyun <99jenny0614@naver.com>
2022-09-28 15:28:45 +0900

    💄 make remove card smaller

--- components/LandingComponents.tsx/RemoveCard.tsx
+++ components/LandingComponents.tsx/RemoveCard.tsx
@@ -16,7 +16,7 @@ const RemoveCard = ({
     <div className="w-landing_mobile md:w-full">
       <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
         <div
-          className={`w-full h-landing_mobile rounded-landing_mobile md:h-[400px]  md:rounded-forImg relative ${
+          className={`w-full h-landing_mobile rounded-landing_mobile md:h-[260px]  md:rounded-forImg relative ${
             isOpened ? "drop-shadow-lg transition-shadow" : ""
           } 
           ${isHover ? "animate-shadow_larger" : "animate-shadow_smaller"}`}>
@@ -32,10 +32,10 @@ const RemoveCard = ({
         className={` ${
           isOpened ? "animate-fadein " : "hidden md:animate-fadeout"
         } flex flex-col items-center justify-center `}>
-        <div className="text-landing_mobile_title border-t-[0.1vh] pt-[2vh] pb-[1.2vh] mt-[2vh] md:text-[34px] md:pt-8 md:pb-4 md:mt-7  md:border-t-[2px] font-bold  border-slate-600 w-[95%] text-center ">
+        <div className="text-landing_mobile_title border-t-[0.1vh] pt-[2vh] pb-[1.2vh] mt-[2vh] md:text-[24px] md:pt-[1.6rem] md:pb-4 md:mt-5  md:border-t-[2px] font-bold  border-slate-600 w-[95%] text-center ">
           {contents.title}
         </div>
-        <div className="text-landing_mobile_content md:text-[20px] md:w-[83%] ">
+        <div className="text-landing_mobile_content md:text-[14.5px]  md:w-[83%] md:leading-[160%]">
           {contents.content}
         </div>
       </div>

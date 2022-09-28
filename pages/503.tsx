72dc006f1c0464d6e5684a18edd31593d499d59f
c-jeongyyun <99jenny0614@naver.com>
2022-09-28 10:35:16 +0900

    ♻️refactor error page

--- pages/503.tsx
+++
@@ -1,4 +0,0 @@
-import Error from "../components/Error";
-
-const Custom503 = () => <Error errCode="503" text="서버에 문제가 발생했습니다 :(" />;
-export default Custom503;
--- pages/505.tsx
+++
@@ -1,4 +0,0 @@
-import Error from "../components/Error";
-
-const Custom505 = () => <Error errCode="505" text="서버에 문제가 발생했습니다 :(" />;
-export default Custom505;

4d41eb8c9ef884f5601fcfe504b79add6c9233bb
c-jeongyyun <99jenny0614@naver.com>
2022-09-30 11:17:12 +0900

    💄 reduce size of LCA arrow

--- components/RAndDComponents/LCA.tsx
+++ components/RAndDComponents/LCA.tsx
@@ -86,33 +86,33 @@ function LCA() {
       <div>
         <div className="flex items-center justify-around">
           <LCACard content={LCAContents.resources} />
-          <ArrowCover className="w-[18px]">
+          <ArrowCover className="w-[15px]">
             <Arrow direction={ArrowDirection.RIGHT} />
           </ArrowCover>
           <LCACard content={LCAContents.processing} />
-          <ArrowCover className="w-[18px]">
+          <ArrowCover className="w-[15px]">
             <Arrow direction={ArrowDirection.RIGHT} />
           </ArrowCover>
           <LCACard content={LCAContents.manufacturing} />
         </div>
         <div className="flex justify-around items-center w-full my-[2px] h-[35px] ">
-          <ArrowCover className="w-[30px]">
+          <ArrowCover className="w-[25px]">
             <Arrow direction={ArrowDirection.UP} />
           </ArrowCover>
           <div className="w-[200px]">
             <Arrow direction={ArrowDirection.NONE} />
           </div>
-          <ArrowCover className="w-[30px]">
+          <ArrowCover className="w-[25px]">
             <Arrow direction={ArrowDirection.DOWN} />
           </ArrowCover>
         </div>
         <div className="flex items-center justify-around">
           <LCACard content={LCAContents.distribution} />
-          <ArrowCover className="w-[18px]">
+          <ArrowCover className="w-[15px]">
             <Arrow direction={ArrowDirection.LEFT} />
           </ArrowCover>
           <LCACard content={LCAContents.use} />
-          <ArrowCover className="w-[18px]">
+          <ArrowCover className="w-[15px]">
             <Arrow direction={ArrowDirection.LEFT} />
           </ArrowCover>
           <LCACard content={LCAContents.endOfLife} />

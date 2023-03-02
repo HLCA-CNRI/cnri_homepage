5f9ca5129f7b409146d5a57780059fecc7687d20
c-jeongyyun <99jenny0614@naver.com>
2023-03-02 15:49:48 +0900

    🌱 create media list as a constant

---
+++ constants/mediaList.ts
@@ -0,0 +1,5 @@
+export default [
+  {name: "네이버 블로그", link: "https://blog.naver.com/cnrikorea_"},
+  {name: "Medium", link: "https://medium.com/@carbonmedia"},
+  {name: "브런치", link: "https://brunch.co.kr/@cnrikorea"},
+];
